207c3473961273e6725de48c2919c53a1029c6d3
18404715032 <20141105066yh@admindeimac-36.local>
2016-07-01 15:10:51 +0800

    Initial Commit

---
+++ mail list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mail list
+//
+//  Created by 杨浩 on 16/7/1.
+//  Copyright © 2016年 杨浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
